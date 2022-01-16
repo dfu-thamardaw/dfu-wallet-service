81946ac722745e65ec9b09505973798742243d32
thuaung30 <thuhtetaung.d2@gmail.com>
2022-01-16 18:54:38 +0630

    Fix /payment

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -1,6 +1,7 @@
-import { Router } from "express"
-import {badRequestHandler} from "../error_handler";
+import { Router } from "express";
+import { badRequestHandler } from "../error_handler";
 import * as client from "../lib";
+import { isEmpty } from "lodash";
 
 const router = Router();
 
@@ -57,26 +58,22 @@ router.post("/", async (req, res, next) => {
   const value = req.body.value;
   const wallet = req.body.wallet;
 
-  if(
-    !networkAddress 
-    || !receiverAddress 
-    || !value
-    || !wallet
-    || !isEmpty(wallet)
-  ){
+  if (
+    !networkAddress ||
+    !receiverAddress ||
+    !value ||
+    !wallet ||
+    isEmpty(wallet)
+  ) {
     const error = new Error("Invalid argument");
     badRequestHandler(error, req, res, next);
   }
-  
+
   try {
     await client.loadAccount(wallet);
-    await client.sendPayment(
-      networkAddress,
-      receiverAddress,
-      value
-    );
+    await client.sendPayment(networkAddress, receiverAddress, value);
     res.json("ok");
-  } catch(err) {
+  } catch (err) {
     next(err);
   }
 });
